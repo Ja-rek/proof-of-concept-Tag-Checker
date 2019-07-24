38ee66f06a3dda4d70751557576c1ac23964a89e
Ja-Rek <dto.jarek@gmail.com>
2019-07-24 03:51:31 +0200

    Add sesssion factory

---
+++ StackOverflow/Infrastructure/Persistence/AutomappingConfiguration.cs
@@ -0,0 +1,14 @@
+using FluentNHibernate;
+using FluentNHibernate.Automapping;
+
+namespace Mediporta.StackOverflow.Infrastructure.Persistence
+{
+    public class AutomappingConfiguration : DefaultAutomappingConfiguration 
+    {
+        public override bool ShouldMap(System.Type type)
+        {
+            return type.Namespace.EndsWith("DataModel");
+        }
+
+    }
+}
---
+++ StackOverflow/Infrastructure/Persistence/SessionFactory.cs
@@ -0,0 +1,31 @@
+﻿using NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using FluentNHibernate.Automapping;
+using Mediporta.StackOverflow.Infrastructure.Persistence.DataModel;
+
+namespace Mediporta.StackOverflow.Infrastructure.Persistence
+{
+    public static class SessionFactory
+    {
+        public static ISession Session(bool createDb = false)
+        {
+            var mySql = "server=localhost;User Id=root; password=Password; database=Mediporta;";
+
+            Action<NHibernate.Cfg.Configuration> shemaConfig = (cfg) => 
+            { 
+                if (createDb) new SchemaExport(cfg).Create(true, createDb); 
+                new SchemaUpdate(cfg).Execute(true, true); 
+            };
+
+            return Fluently.Configure()
+                .Database(MySQLConfiguration.Standard.ConnectionString(mySql).ShowSql())
+                .Mappings(m => m.AutoMappings.Add(AutoMap.AssemblyOf<TagData>(new AutomappingConfiguration())))
+                .ExposeConfiguration(shemaConfig)
+                .BuildSessionFactory()
+                .OpenSession();
+        }
+    }
+}
