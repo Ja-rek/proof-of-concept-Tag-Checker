fba917b2aa378e07ef6808d8b381c0ce64454375
Ja-Rek <dto.jarek@gmail.com>
2019-07-24 03:52:18 +0200

    Add logger config

---
+++ StackOverflow/Infrastructure/LogConfig.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Mediporta.StackOverflow.Infrastructure
+{
+    public static class LogConfig
+    {
+        public static LoggingConfiguration Config()
+        {
+            var config = new LoggingConfiguration();
+
+            var logfile = new FileTarget("logfile") { FileName = "log.txt" };
+            var console = new ColoredConsoleTarget()
+            {
+                Layout = "${date:format=HH\\:mm\\:ss} ${level}: \n ${message} ${exception}\n"
+            };
+
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, logfile);
+            config.AddRule(LogLevel.Trace, LogLevel.Fatal, console);
+                        
+            return config;
+        }
+    }
+}
